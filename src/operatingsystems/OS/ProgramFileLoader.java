23e5b4a15a5f63dac1f830bcce53bd86c548f578
ohdabbs <odabbs@students.kennesaw.edu>
2017-03-05 21:00:44 -0500

    Initial Build
    Creates a Program object for each program in source file.  Adds each
    program to the ArrayList programs.  Sets the pid, instructionCount,
    diskAddress, inputBufferSize, outputBufferSize, and tempBufferSize for
    each Program object.  Loads all hex code instructions into disk,
    starting at 0.  A Program objects instructions can be found from its
    diskAddress to diskAddress + instructionCount.  A programs input buffer
    can be found from its diskAddress + instructionCount to diskAddress +
    instructionCount + inputBufferSize.  Follow the same procedure for
    output and temp buffers.

--- src/operatingsystems/OS/ProgramFileLoader.java
+++ src/operatingsystems/OS/ProgramFileLoader.java
@@ -28,6 +28,52 @@ public class ProgramFileLoader {
      */
     public void load(String filename) {
 	this.file = new File(filename);
+	try{
+		BufferedReader br = new BufferedReader (new FileReader(filename));
+			String line;
+			int programno = 0; //current pcb
+			int lineno = 0; //current line of source file
+			int instructionno = 0; //current instruction number (excludes lines that begin with '//')
+			
+			try{
+				while((line = br.readLine()) != null){	
+					
+					if(line.contains("JOB")){
+						String[] s = line.split(" ");
+						Program p = new Program();
+						programs.add(p);
+						p.pid = Integer.parseInt(s[2], 16);
+						p.instructionCount = Integer.parseInt(s[3], 16);
+						p.diskAddress = instructionno;
+						programno++;
+					}
+					else if(line.contains("Data")){
+						String[] s = line.split(" ");
+						programs.get(programno-1).inputBufferSize = Integer.parseInt(s[2], 16);
+						programs.get(programno-1).outputBufferSize = Integer.parseInt(s[3], 16);
+						programs.get(programno-1).tempBufferSize = Integer.parseInt(s[4], 16);
+					}
+					else if(line.contains("END")){
+						
+					}
+					else{
+						line = line.replace(" ", "");
+						line = line.substring(2);
+						long b = Long.parseLong(line, 16);
+						int a = (int)b;
+						d.write(instructionno, a);
+						instructionno++;
+					}
+					lineno++;
+				}
+				//System.out.println(lineno);
+			}catch(IOException ex){
+				System.out.println("IOException");
+			}
+		}catch(FileNotFoundException ex){
+			System.out.println("File not found");
+		}
+    }
 	
 	throw new NotImplementedException();
     }
